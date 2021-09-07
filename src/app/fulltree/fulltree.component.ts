4dd842b321d9689dad1a4b930a41753a2ff6a73f
Lakshay Narang <lakshay.narang03@gmail.com>
2021-09-07 15:18:13 +0530

    Changes for the Tree Component

---
+++ src/app/fulltree/fulltree.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fulltree',
+  templateUrl: './fulltree.component.html',
+  styleUrls: ['./fulltree.component.css']
+})
+export class FulltreeComponent {
+  nodes = [
+    {
+      "name": "countries",
+      "children": [
+        {
+          "name": "india",
+          "children": [
+            {
+              "name": "zones",
+              "children": [
+                {
+                  "name": "north",
+                  "children": [
+                    {
+                      "name": "states",
+                      "children": [
+                        {
+                          "name": "punjab",
+                          "children": [
+                            {
+                              "value": "population",
+                              "name": "28000000"
+                            },
+                            {
+                              "name": "J&K",
+                              "children": [
+                                {
+                                  "value": "population",
+                                  "name": "1000000"
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "name": "south",
+                  "children": [
+                    {
+                      "name": "states",
+                      "children": [
+                        {
+                          "name": "tamil nadu",
+                          "children": [
+                            {
+                              "value": "population",
+                              "name": "67000000"
+                            },
+                            {
+                              "name": "kerala",
+                              "children": [
+                                {
+                                  "value": "population",
+                                  "name": "34000000"
+                                }
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
+}
