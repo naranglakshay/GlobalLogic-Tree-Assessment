ab5aa24f4229f979dc356e9fbc531b8da04b03df
Lakshay Narang <lakshay.narang03@gmail.com>
2021-09-08 23:30:57 +0530

    Fulltree spec ts commit

--- src/app/fulltree/fulltree.spec.ts
+++ src/app/fulltree/fulltree.spec.ts
@@ -1,1 +1,17 @@
 
+import { TestBed, async } from '@angular/core/testing';
+import { FulltreeComponent } from './fulltree.component';
+describe('FulltreeComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        FulltreeComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should create the 
+     ', async(() => {
+    const fixture = TestBed.createComponent(FulltreeComponent);
+    expect(fixture).toBeTruthy();
+  }));
+});
